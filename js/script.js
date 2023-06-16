6343505a85e0a9c4d870b49bc3c7e6d1f83258e5
ChristianMazzaro <christianmazzaro2@gmail.com>
2023-06-16 22:54:36 +0200

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -21,7 +21,6 @@ for (let i = 0; i < imageList.length; i++){
     const newImg = document.createElement("div");
     
     if(i === 0 ) {
-
         newImg.innerHTML = `<img src="${imageList[i]}">`;
         newImg.classList.add("_image");
         newImg.classList.add("selected");
@@ -36,14 +35,62 @@ for (let i = 0; i < imageList.length; i++){
     console.log(i);
 
     imageCarousel.append(newImg);
+
 }
+
+let j = 0;
 
 const _Button = document.getElementById('_Button');
 
 _Button.addEventListener('click', function() {
 
-    alert('cliccato');
-   
-    
+    j += 1;
 
+    alert('cliccato');
+
+    //sto bastardo deve capire che deve mettere la classe all'immagine dopo togliendola a quella prima
+    //abbiamo un modo per definirle? si, l'array
+    //e ora che le conosciamo che cazzo ci facciamo?
+
+
+        let next = document.getElementsByClassName(`_image`);
+
+        console.log(next);
+        
+        
+            if(j === 1 ) {
+
+                
+                next[0].classList.remove('selected');
+                next[1].classList.add('selected');
+                console.log(i);
+                
+            }
+            
+            else if(j === 2 ){
+                
+                
+                next[1].classList.remove('selected');
+                next[2].classList.add('selected');
+                console.log(i);
+
+            }
+
+            else if(j === 3 ){
+                
+                
+                next[2].classList.remove('selected');
+                next[3].classList.add('selected');
+                console.log(i);
+
+            }
+
+            else{
+
+                next[3].classList.remove('selected');
+                next[4].classList.add('selected');
+                console.log(i);
+
+            }
+    
 });
