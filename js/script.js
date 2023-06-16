28a4953f2453519affd2030d7ba86e8ff6258458
ChristianMazzaro <christianmazzaro2@gmail.com>
2023-06-16 17:41:11 +0200

    commit 3

--- js/script.js
+++ js/script.js
@@ -4,28 +4,46 @@
 
 //devo fare un array con le immagini, pero per farlo devo prima trasformarle in variabili
 
-const imageList = ["img/01.webp","img/02.webp","img/03.webp","img/04.webp","img/05.webp"];
+const imageList = [
+    "img/01.webp",
+    "img/02.webp",
+    "img/03.webp",
+    "img/04.webp",
+    "img/05.webp"
+];
 
 const imageCarousel = document.getElementById(`imageCarousel`)
 
-
-console.log(imageList[0]);
-
-console.log(imageList[1]);
-
-console.log(imageList[2]);
-
-console.log(imageList[3]);
-
-console.log(imageList[4]);
+console.log(imageList);
 
 for (let i = 0; i < imageList.length; i++){
 
     const newImg = document.createElement("div");
-    newImg.classList.add("_image");
-    newImg.innerHTML = '' + `<img src="${imageList[i]}">`;
+    
+    if(i === 0 ) {
+
+        newImg.innerHTML = `<img src="${imageList[i]}">`;
+        newImg.classList.add("_image");
+        newImg.classList.add("selected");
+
+    }
+      
+    else{
+        newImg.innerHTML = `<img src="${imageList[i]}">`;
+        newImg.classList.add("_image");
+    }
 
     console.log(i);
 
     imageCarousel.append(newImg);
 }
+
+const _Button = document.getElementById('_Button');
+
+_Button.addEventListener('click', function() {
+
+    alert('cliccato');
+   
+    
+
+});
